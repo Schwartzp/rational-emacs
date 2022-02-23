1a07dcb96b605ead14b451c3f89dee3b57df62da
Schwartz Pierre <schwartz.pierre39@gmail.com>
2022-02-23 08:22:16 +0100

    keep scrollbar

--- early-init.el
+++ early-init.el
@@ -25,7 +25,7 @@
 (setq inhibit-startup-message t)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+;;(push '(vertical-scroll-bars) default-frame-alist)
 (push '(background-color . "#232635") default-frame-alist)
 (push '(foreground-color . "#FFFFFF") default-frame-alist)
 (push '(mouse-color . "white") default-frame-alist)
